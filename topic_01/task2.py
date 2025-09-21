b469138885d86cc66b559f5f87444b8167984a25
HerasimenkoVika <sirius.gvr@gmail.com>
2025-09-22 00:23:19 +0300

    all is good now

---
+++ topic_01/task2.py
@@ -0,0 +1,15 @@
+s = "   Hello World   "
+print(s.strip())
+print("...World...".strip("."));
+
+s = "hello World"
+print(s.capitalize());
+
+s = "hello World"
+print(s.title());
+
+s = "Hello World"
+print(s.upper());
+
+s = "Hello World"
+print(s.lower());
